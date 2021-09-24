addf680bee671fe2712771ce724a7b1ffb6b6fd1
ujjwalkumar93 <pathakujjwal93@gmail.com>
2021-09-25 04:10:50 +0530

    customer enequiet workspace and api added

---
+++ pastech_app/api.py
@@ -0,0 +1,14 @@
+import frappe
+
+@frappe.whitelist(allow_guest=True)
+def create_customer_enquiry(name,phone,email,data):
+    doc = frappe.new_doc("Customer Enquiry")
+    doc.name = name
+    doc.mobile = phone
+    doc.email = email
+    doc.enquiry = data
+    doc.insert()
+    if doc.get("name"):
+        return True
+    else:
+        return False
